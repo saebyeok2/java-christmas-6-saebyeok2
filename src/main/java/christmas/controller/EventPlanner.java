21c994bf3e65a2e4d8d31b70ca0e6696ca5173d2
saebyeok2 <tomato8269@naver.com>
2023-11-14 15:08:56 +0900

    feat: 식당 방문 날짜 입력 기능 구현
    - 사용자로부터 식당 방문 날짜를 입력받는다. (1~31일)

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,4 +1,11 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+
 public class EventPlanner {
+    InputView inputView = new InputView();
+
+    public void run() {
+        int date = inputView.readDate();
+    }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,8 @@
 package christmas.view;
 
 public class OutputView {
+    public void printMenu() {
+        System.out.println("<주문 메뉴>");
+        // ...
+    }
 }
