236dbe8df4f8eaff01c8720f8c716a590297f208
saebyeok2 <tomato8269@naver.com>
2023-11-14 17:02:41 +0900

    feat: 방문 날짜 입력 예외 처리
    - 입력받은 방문 날짜가 정수가 아닌 경우
    - 방문 날짜가 1 이상 31 이하의 숫자가 아닌 경우

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,11 +1,38 @@
 package christmas.controller;
 
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class EventPlanner {
     InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
 
     public void run() {
-        int date = inputView.readDate();
+        int date = inputDate();
     }
+
+    private int inputDate() {
+        try {
+            String input = inputView.readDate();
+            return validate(input);
+        } catch (IllegalArgumentException e) {
+            outputView.printInputDateError();
+            return inputDate();
+        }
+    }
+
+    private int validate(String input) {
+        try {
+            int date = Integer.parseInt(input);
+
+            if (date < 0 || 31 < date) {
+                throw new IllegalArgumentException();
+            }
+
+            return date;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
         System.out.println("<주문 메뉴>");
         // ...
     }
+
+    public void printInputDateError() {
+        System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.");
+    }
 }
