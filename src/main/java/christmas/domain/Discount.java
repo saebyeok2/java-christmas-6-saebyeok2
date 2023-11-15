949a3b9342a5df0501d9309b902c644725253cae
saebyeok2 <tomato8269@naver.com>
2023-11-15 15:19:12 +0900

    feat: 크리스마스 디데이 할인 기능 구현
    - 1일부터 25일까지 1000원부터 시작하여 크리스마스에 가까워질수록 100원씩 할인 금액이 늘어난다.

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -3,7 +3,20 @@ package christmas.domain;
 import java.util.Map;
 
 public class Discount {
-    public static void calculateDiscount(Map<Menu, Integer> order, int orderDate) {
-        
+    public static void calculateDiscount(int orderDate, Map<Menu, Integer> order) {
+        int christmasDiscount = calculateChristmasDiscount(orderDate);
     }
+
+    private static int calculateChristmasDiscount(int orderDate) {
+        int christmasStart = 1;
+        int christmasEnd = 25;
+        int discountAmount = 0;
+
+        if (orderDate >= christmasStart && orderDate <= christmasEnd) {
+            discountAmount = 1000 + (orderDate - 1) * 100;
+        }
+
+        return discountAmount;
+    }
+
 }
